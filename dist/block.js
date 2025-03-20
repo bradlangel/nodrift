5e6710decd54bbcdd000b02d8ea930b5492e17ca
bradlangel <b.a.langel@gmail.com>
2025-03-20 14:35:30 -0400

    [no-distractions-chrome-extension] website blocker chrome extension
    Wanted to create a custom website blocker since nothing has worked out
      so far. By doing this it makes it a bit harder to disable sites and
      also I can add my custom touch to a simple thing like a redirect. It
      displays a page with a link to my career tracker site. It's good
      enough for now.

---
+++ dist/block.js
@@ -0,0 +1,28 @@
+const blockedSites = ["reddit.com", "old.reddit.com", "www.reddit.com"];
+const buildRules = (sites) => {
+    return sites.map((site, idx) => ({
+        id: idx + 1,
+        priority: 1,
+        action: {
+            type: chrome.declarativeNetRequest.RuleActionType.REDIRECT,
+            redirect: {
+                extensionPath: `/block.html?blockedUrl=${encodeURIComponent('{url}')}`
+            },
+        },
+        condition: {
+            urlFilter: `*://${site}/*`,
+            resourceTypes: [chrome.declarativeNetRequest.ResourceType.MAIN_FRAME],
+        },
+    }));
+};
+console.log('Website blocker: Service Worker Loaded');
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: blockedSites.map((_, idx) => idx + 1),
+        addRules: buildRules(blockedSites),
+    }, () => {
+        chrome.declarativeNetRequest.getDynamicRules((rules) => {
+            console.log('Dynamic rules have been updated:', rules);
+        });
+    });
+});
---
+++ redirect.js
@@ -0,0 +1,6 @@
+// redirect.js
+function redirectToCareer() {
+  window.location = 'http://localhost:5173';
+}
+
+document.getElementById('redirect-btn').addEventListener('click', redirectToCareer);
